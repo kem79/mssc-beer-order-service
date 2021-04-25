5440cd84094f0ad5dd0604471b0b046092bc8542
Marc <marc.marechal@dell.com>
2021-04-25 12:12:26 +0800

    fix UPCs used to order beer

--- src/main/java/guru/sfg/beer/order/service/bootstrap/BeerOrderBootStrap.java
+++ src/main/java/guru/sfg/beer/order/service/bootstrap/BeerOrderBootStrap.java
@@ -18,8 +18,8 @@ import java.util.UUID;
 public class BeerOrderBootStrap implements CommandLineRunner {
     public static final String TASTING_ROOM = "Tasting Room";
     public static final String BEER_1_UPC = "0631234200036";
-    public static final String BEER_2_UPC = "0631234300019";
-    public static final String BEER_3_UPC = "0083783375213";
+    public static final String BEER_2_UPC = "0631234200037";
+    public static final String BEER_3_UPC = "0631234200038";
 
     private final CustomerRepository customerRepository;
 
